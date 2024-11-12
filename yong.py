2a24885c509cbed178d766b0cfced193f6d231d6
snaptoover57 <snaptoover57@gmail.com>
2024-11-13 00:55:58 +0900

    Delete yong.py

--- yong.py
+++ yong.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 ### infostar github test ###
-=======
-### infostar github test ###
->>>>>>> 2a9a4a7ddbe9f4c53b17d162d7c0ce95c738207c
 print('infostar')
+print('second')
+print('third')
