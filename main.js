89fa1102ec2087d6c3f8a891348dbeabab142a2e
seoyeonssi <160923153+seoyeonssi@users.noreply.github.com>
2025-02-24 12:53:26 +0900

    최종프론트엔드

---
+++ main.js
@@ -0,0 +1,37 @@
+// 로그인 버튼 기능 
+document.querySelector('.login').addEventListener('click', function() {
+    window.location.href = 'login.html'; // 로그인 페이지로 이동
+});
+
+// 회원가입 버튼 기능
+document.querySelector('.signup').addEventListener('click', function() {
+    window.location.href = 'join.html'; // 회원가입 페이지로 이동
+});
+
+// 홈화면 버튼 기능
+document.querySelector('.title-container').addEventListener('click', function() {
+    window.location.href = '홈화면2.html'; // 홈화면 페이지로 이동
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
